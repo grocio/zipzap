a948052dce79d077ec92ba35330bf930d4b510d5
Sho Ishiguro <ishiguro.sho.grocio@gmail.com>
2019-03-11 13:05:09 +0900

    Add files via upload

---
+++ zipzap.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+#! /usr/bin/env python
+
+target_extension = '.wav'
+
+import os
+import zipfile
+
+current_path = os.getcwd()
+folder_name = os.path.basename(current_path)
+file_name_li = os.listdir(current_path)
+
+with zipfile.ZipFile('comp_{}.zip'.format(folder_name), 'w') as new_zip:
+	for file_name in file_name_li: 
+		if file_name.endswith(target_extension):
+			new_zip.write(file_name)
